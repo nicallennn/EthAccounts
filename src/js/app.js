d72f9ec99430f049672d20bec0567b37a792fdc6
Nick Allen <nick@scapplications.co.uk>
2018-03-23 00:38:16 +0000

    jobs, employees frontend

--- src/js/app.js
+++ src/js/app.js
@@ -88,7 +88,7 @@ App = {
 
             //add event eventListener
             App.eventListener();
-
+            App.reloadEmployees();
             //retrieve job from contract
             return App.reloadJobs();
 
@@ -97,7 +97,7 @@ App = {
 
 /***********                   **************                   ************/
 
-    // CONTRACT FUNCTIONS
+    /************************* JOB FUNCTIONS *************************/
 
     reloadJobs: function() {
       //check reentry
@@ -161,7 +161,7 @@ App = {
 
        var ethPrice = web3.fromWei(price, "ether");
 
-
+       //append the data
        $('#jobsAdminTable').append('<tr><td>'
        + name +
        '</td><td>'
@@ -229,8 +229,6 @@ App = {
 
     },
 
-
-
     payJob: function() {
       //block default events
       event.preventDefault();
@@ -277,38 +275,180 @@ App = {
 
     },
 
+
+
+    /************************* EMPLOYEE FUNCTIONS *************************/
+
+    reloadEmployees: function() {
+      //check reentry
+      if(App.loading) {
+        return;
+      }
+      App.loading = true;
+
+       //refresh account info
+       App.displayAccountInfo();
+
+       //store instance of contract
+       var ethAccountsInstance;
+
+       // //get the job placeholder
+        $('#employeeRow').empty();
+
+       App.contracts.ethAccounts.deployed().then(function(instance) {
+         //store contact instance
+         ethAccountsInstance = instance;
+         //get unpaid jobs
+        return ethAccountsInstance.getMyEmployees();
+      }).then(function(employeeIds){
+        //clear the job fromWei
+        $('#employeesRow').empty();
+
+        //iterate over jobIds array
+        for(var i = 0; i < employeeIds.length; i++){
+          var empId = employeeIds[i];
+          ethAccountsInstance.employees(empId.toNumber()).then(function(employee) {
+            App.displayEmployee(employee[0], employee[3], employee[6], employee[7], employee[4], employee[5]);           /////////////////////////
+          });
+
+        }
+
+        App.loading = false;
+        var emps = $('#employees');
+        $('#employeesRow').append(emps.html());
+
+      }).catch(function(err) {
+        console.error(err.message);
+        App.loading = false;
+      });
+    },
+
+    displayEmployee: function(id, name, postal, phone, salary, lastPaid){
+      var ethSalary = web3.fromWei(salary, "ether");
+
+      $('#employeeTable').append('<tr><td>'
+      + name +
+      '</td><td>'
+      + postal+
+      '</td><td>'
+      + phone +
+      '</td><td>'
+      + ethSalary +
+      '</td><td>'
+      + lastPaid +
+      '</td><td><button type="button" class="btn btn-default btn-pay" data-id="{' + id +'}" data-salary="{' + ethSalary + '}" onclick="App.payEmployee(); return false;">Pay</button></td></tr>');
+
+    },
+
+    addEmployee: function(){
+      //get the details of the new employee
+      var _emp_address = $('#emp_address').val();
+      var _emp_name = $('#emp_name').val();
+      var _emp_salary = web3.toWei(parseFloat($('#emp_sal').val() || 0));
+      var _emp_post = $('#emp_post').val();
+      var _emp_phone = $('#emp_phone').val();
+
+      //check for employee
+      if((_emp_name.trim() == '') || (_emp_salary == 0)) {
+        //no new employee
+        return false;
+      }
+
+      //get instance of that contract and call addEmployee function
+      App.contracts.ethAccounts.deployed().then(function(instance) {
+        return instance.addEmployee(_emp_address, _emp_name, _emp_salary, _emp_post, _emp_phone, {
+          //metadata for function
+          from: App.account,
+          gas: 600000,
+        });
+      }).then(function(result) {
+
+        //catch any errors
+      }).catch(function(err) {
+        //log errors
+        console.error(err);
+      });
+
+    },
+
+    payEmployee: function() {
+      //block default events
+      event.preventDefault();
+
+      //get id
+      var _employeeId = $(event.target).data('id');
+
+
+
+      //get the job price. event.target is the button clicked
+      var _salary = $(event.target).data('salary');
+
+      //remove all non-numeric chars
+      _salary = _salary.replace(/[^0-9\.]+/g, "");
+
+      _employeeId = _employeeId.replace(/[^0-9\.]+/g, "");
+
+      //parse to float
+      _salary = parseFloat(_salary);
+
+      //parse to int
+      _employeeId = parseFloat(_employeeId);
+
+      //get utc date
+      _date = $date.toUTCString();
+
+      //call the payJob function
+      App.contracts.ethAccounts.deployed().then(function(instance) {
+        return instance.payEmployee(_employeeId, _date, {
+          //metadata for function
+          from: App.account,
+          value: web3.toWei(_salary, "ether"),
+          gas: 600000
+        }).then(function(result) {
+
+          //catch any errors
+        }).catch(function(err) {
+          //log errors
+          console.error(err);
+        });
+      });
+    },
+
+    /************************* RESOURCE FUNCTIONS *************************/
+
+    
+    /************************* HISTORY FUNCTIONS *************************/
+
+    /************************* TAX FUNCTIONS *************************/
+
 /***********                   **************                   ************/
 
-    // EVENTS
+    //EVENTS
     eventListener: function() {
         //get instance of deployed contact
         App.contracts.ethAccounts.deployed().then(function(instance) {
+
+          //JOB EVENTS
           instance.LogAddJob({}, {}).watch(function(error, event) {
-            //check for errors
-            if(!error) {
-              //add events to list on page
-              $("#events").append('<li class="list-group-item"> ' + event.args._name + ' has been added.</li>')
-            } else {
-              console.error(error);
-            }
-            App.reloadJobs();
-          });
-          //add event listener
-          instance.LogPayJob({}, {}).watch(function(error, event) {
-            //check for errors
-            if(!error) {
-              //add events to list on page
-              $("#events").append('<li class="list-group-item"> ' + event.args._client + ' client paid ' + event.args._admin +
-                ' ' + event.args._price + ' for '  + event.args._name + '</li>')
-            } else {
-              console.error(error);
-            }
             App.reloadJobs();
           });
 
+          instance.LogPayJob({}, {}).watch(function(error, event) {
+
+            App.reloadJobs();
+          });
+
+          //EMPLOYEE EVENTS
+          instance.LogAddEmployee({}, {}).watch(function(error, event) {
+
+            App.reloadEmployees();
+          });
+          instance.LogPayEmployee({}, {}).watch(function(error, event) {
+
+            App.reloadEmployees();
+          });
         });
     }
-
 };
 
 $(function() {
